0ef208f42442cdd8fff3ff2e531adfff0c932c8f
Raihan <mdr8077@gmail.com>
2025-11-11 04:12:46 +0600

    Perfect home Section done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import Slider from '../../Components/Slider/Slider';
+import React from "react";
+import Slider from "../../Components/Slider/Slider";
+import PerfectHome from "../../Components/PerfectHome/PerfectHome";
 
 const Home = () => {
-    return (
+  return (
+    <div>
       <section>
         <Slider />
       </section>
-    );
+      <section>
+
+      </section>
+      <section>
+        <PerfectHome/>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
